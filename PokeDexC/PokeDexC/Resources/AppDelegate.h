8ffe00ae40bb3738328f5acf4cd0570b0fe4c0bc
Mitch Merrell <mitchmerrell@Mitchs-MacBook-Pro.local>
2019-08-20 09:09:04 -0600

    initial commit

---
+++ PokeDexC/PokeDexC/Resources/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokeDexC
+//
+//  Created by Mitch Merrell on 8/20/19.
+//  Copyright © 2019 Mitch Merrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PokeDexC/PokeDexC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PokeDexC
+//
+//  Created by Mitch Merrell on 8/20/19.
+//  Copyright © 2019 Mitch Merrell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
